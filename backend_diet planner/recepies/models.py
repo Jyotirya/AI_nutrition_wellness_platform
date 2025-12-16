ec5e113ef9506cfc7194c320be8ff7f489ad2338
culotis <ishantkumar893@gmail.com>
2025-12-16 10:57:35 +0530

    backend for diet planner

---
+++ backend_diet planner/recepies/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Recipe(models.Model):
+    name = models.CharField(max_length=200)
+    calories = models.FloatField()
+    protein_g = models.FloatField()
+    carbs_g = models.FloatField()
+    fats_g = models.FloatField()
+    
+    # Metadata for filtering
+    is_vegetarian = models.BooleanField(default=False)
+    contains_nuts = models.BooleanField(default=False)
+    cuisine = models.CharField(max_length=50) # e.g., "Indian"
+    meal_type = models.CharField(max_length=20) # "Breakfast", "Lunch", "Dinner", "Snack"
+
+    def __str__(self):
+        return self.name
